5f68b1b95d9a0dd365de1286d8d96ce067cc9644
Jose Luis <jlcgmieres15@gmail.com>
2026-09-13 15:51:30 +0200

    Testing building of a simple house

--- src/test/java/com/jose/builds/HouseBuilderTest.java
+++ src/test/java/com/jose/builds/HouseBuilderTest.java
@@ -1,6 +1,11 @@
 package com.jose.builds;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class HouseBuilderTest {
   private HouseBuilder builder;
@@ -8,5 +13,23 @@ public class HouseBuilderTest {
   @BeforeEach
   void setUp() {
     builder = new ConcreteHouseBuilder();
+  }
+
+  @Test
+  void testBuildSimpleHouse() {
+    House house = builder.setWalls(4)
+        .setDoors(1)
+        .setWindows(2)
+        .setRoof(true)
+        .build();
+
+    assertEquals(4, house.getWalls());
+    assertEquals(1, house.getDoors());
+    assertEquals(2, house.getWindows());
+    assertTrue(house.hasRoof());
+    assertFalse(house.hasGarage());
+    assertFalse(house.hasGarden());
+    assertFalse(house.hasSwimmingPool());
+    assertEquals(0, house.getStatuesCount());
   }
 }
