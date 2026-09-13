8ea349cfc462e6fe5710f86497889b52389dfcd8
Jose Luis <jlcgmieres15@gmail.com>
2026-09-13 15:44:39 +0200

    Engineer builds a luxury house

--- src/main/java/com/jose/builds/CivilEngineer.java
+++ src/main/java/com/jose/builds/CivilEngineer.java
@@ -11,4 +11,11 @@ public class CivilEngineer {
     return builder.setWalls(4).setDoors(1)
             .setDoors(2).setRoof(true).build();
   }
+
+  public House constructLuxuryHouse() {
+    return builder.setWalls(8).setDoors(4)
+            .setWindows(18).setRoof(true)
+            .setGarage(true).setGarden(true)
+            .setStatuesCount(4).build();
+  }
 }
